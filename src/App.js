5e56c6339f1d30ef180ce8c2125d1e41ba2203be
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-07 21:41:19 +0100

    add particle js as background

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {
         /*
-        <ImageLinkForm/>
-        <FaceRecognition/>*/
+          <FaceRecognition/>*/
       }
     </div>
   );
