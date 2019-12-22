cd51ae9b9c1a88eb42d671c48fba1dd8e07e8e9d
clehr <christian_lehr@outlook.de>
2019-12-22 22:58:21 +0100

    update user count

--- src/App.js
+++ src/App.js
@@ -84,7 +84,22 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input
-    ).then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    ).then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+      .then(response => response.json())
+      .then(count => {
+        this.setState(Object.assign(this.state.user, { entries: count }))
+      })
       .catch(err => console.log(err));
   }
 
@@ -107,12 +122,12 @@ class App extends Component {
         {route === 'home'
           ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onImageSubmit} />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
           : (route === 'signin'
-            ? <Signin onRouteChange={this.onRouteChange} />
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
